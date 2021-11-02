8bc7bb833768baf4d8cc27c7ba3c90a54bf55c4b
George W <georgewigley123@gmail.com>
2021-11-02 15:32:09 +0000

    Created hello world spring boot project using start.spring.io

---
+++ SpringBoot/planningInspectorate/src/main/java/com/planningInspectorate/PlanningInspectorateApplication.java
@@ -0,0 +1,13 @@
+package com.planningInspectorate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanningInspectorateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanningInspectorateApplication.class, args);
+	}
+
+}
