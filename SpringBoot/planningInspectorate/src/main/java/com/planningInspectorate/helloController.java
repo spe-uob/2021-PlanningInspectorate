6c094ba25668d880273ee7e4b55c68b251c03b56
George W <georgewigley123@gmail.com>
2021-11-10 15:12:35 +0000

    Added custom parameter to hello world controller, to use add ?variable=value e.g. localhost:8080?name=george

--- SpringBoot/planningInspectorate/src/main/java/com/planningInspectorate/helloController.java
+++ SpringBoot/planningInspectorate/src/main/java/com/planningInspectorate/helloController.java
@@ -3,14 +3,15 @@ import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.PropertySources;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class helloController {
     @GetMapping("/")
     @ResponseBody
-    public String helloWorld(){
-        return "Hello world from planning inspectorate project!";
+    public String helloWorld(@RequestParam(defaultValue = "world", required = false) String name){
+        return "Hello " + name + " from planning inspectorate project!";
     }
 
 }
