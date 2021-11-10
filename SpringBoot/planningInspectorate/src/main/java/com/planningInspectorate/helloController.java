ff0bddda7e1d68b3353aaa6cb2d2cb0a7828efa0
George W <georgewigley123@gmail.com>
2021-11-10 15:43:30 +0000

    task 6 of hello world done in pair programming with Imran

--- SpringBoot/planningInspectorate/src/main/java/com/planningInspectorate/helloController.java
+++ SpringBoot/planningInspectorate/src/main/java/com/planningInspectorate/helloController.java
@@ -2,6 +2,7 @@ package com.planningInspectorate;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.PropertySources;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -9,9 +10,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class helloController {
     @GetMapping("/")
-    @ResponseBody
-    public String helloWorld(@RequestParam(defaultValue = "world", required = false) String name){
-        return "Hello " + name + " from planning inspectorate project!";
+    public String helloWorld(@RequestParam(defaultValue = "world", required = false) String name, Model model){
+        model.addAttribute("name",name);
+        return "hello";
     }
 
 }
