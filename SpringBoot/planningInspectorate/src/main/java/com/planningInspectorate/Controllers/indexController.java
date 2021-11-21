38b69404abfd8210b8cd232768d1056dcfbf842e
George W <georgewigley123@gmail.com>
2021-11-21 22:22:41 +0000

    Created controller for web pages and created templates for each, all linked together ready to have content added

--- SpringBoot/planningInspectorate/src/main/java/com/planningInspectorate/Controllers/indexController.java
+++ SpringBoot/planningInspectorate/src/main/java/com/planningInspectorate/Controllers/indexController.java
@@ -2,12 +2,29 @@ package com.planningInspectorate.Controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+
+// Controller for basic web requests to the various pages of the site
 @Controller
 public class indexController {
     @GetMapping("/")
     public String GetMainPage(){
         return "index";
+    }
 
+    @GetMapping("/newConsultationList")
+    public String GetNewConsListPage(){
+        return "createNewConsList";
+    }
+
+    @GetMapping("/manageDatabase")
+    public String GetManageDBPage(){
+        return "manageDatabase";
+    }
+
+    @GetMapping("/formResponses")
+    public String GetCreatePage(){
+        return "viewFormResponses";
     }
 }
