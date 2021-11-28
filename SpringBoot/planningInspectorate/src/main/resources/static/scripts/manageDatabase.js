a98a49d36f207342d3b13785e6568b0bdf102a25
George W <georgewigley123@gmail.com>
2021-11-28 14:17:47 +0000

    Fixed popup

--- SpringBoot/planningInspectorate/src/main/resources/static/scripts/manageDatabase.js
+++ SpringBoot/planningInspectorate/src/main/resources/static/scripts/manageDatabase.js
@@ -1,19 +1,64 @@
-// Get the modal
-var modal = document.getElementById("edit-record-popup");
 
-// Get the button that opens the modal
-var openBtn = document.getElementsByClassName("dialog-button");
+let textFieldTemplate = `<form action="#">
+                                <div class="mdl-textfield mdl-js-textfield">
+                                    <input class="mdl-textfield__input" type="text" id="sample1">
+                                    <label class="mdl-textfield__label" for="sample1">Txxt...</label>
+                                </div>
+                          </form>`;
 
-// Get the <span> element that closes the modal
-var closeBtn = document.getElementById("close-popup-button");
+function CreateTableRow(data) {
+    let row = document.createElement("tr");
+    for (let value of data) {
+        let cell = document.createElement("td");
+        cell.className = "mdl-data-table__cell--non-numeric";
+        cell.innerHTML = value;
+        row.appendChild(cell);
+    }
+    return row;
+}
 
-// When the user clicks on the button, open the modal
-for (let i = 0; i < openBtn.length; i++) {
-    openBtn[i].onclick = function () {
-        modal.style.display = "block";
+
+function SetupEditRecordPopup() {
+// Get the popup element
+    let popup = document.getElementById("edit-record-popup");
+// Get the buttons that open the popup
+    let openBtn = document.getElementsByClassName("dialog-button");
+// Get the <button> element that closes the popup
+    let closeBtn = document.getElementById("close-popup-button");
+// When the user clicks on the button, open the popup
+    for (let i = 0; i < openBtn.length; i++) {
+        openBtn[i].onclick = function () {
+            // display popup
+            popup.style.display = "block";
+
+            let popupTableBody = document.getElementById("popup-table-body");
+            popupTableBody.innerHTML = "";
+
+
+            let firstColumn = [];
+            // get column names for the table
+            let tableHead = document.getElementById("database-table-head");
+            for (let heading of tableHead.querySelectorAll("th")){
+                // if header isn't blank
+                if (heading.innerHTML !== "") {
+                    firstColumn.push(heading.innerHTML);
+                }
+            }
+
+            let secondColumn = [];
+            let tableRow = document.getElementById("database-table-body").querySelectorAll("tr")[i];
+            for (let dataCell of tableRow.querySelectorAll("td")){
+                secondColumn.push(dataCell.innerHTML);
+            }
+
+            for (let j = 0; j < firstColumn.length; j++) {
+                popupTableBody.appendChild(CreateTableRow([firstColumn[j], secondColumn[j]]));
+            }
+        }
+    }
+    closeBtn.onclick = function () {
+        popup.style.display = "none";
     }
 }
 
-closeBtn.onclick = function() {
-    modal.style.display = "none";
-}
+SetupEditRecordPopup();
