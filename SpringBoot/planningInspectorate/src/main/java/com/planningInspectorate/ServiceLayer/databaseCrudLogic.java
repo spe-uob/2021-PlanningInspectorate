ac86e61d6e23f4a6622c7dad737cec75c22c2096
Manami <manami2622@gmail.com>
2021-12-06 11:16:23 +0000

    try to add GetRecord

--- SpringBoot/planningInspectorate/src/main/java/com/planningInspectorate/ServiceLayer/databaseCrudLogic.java
+++ SpringBoot/planningInspectorate/src/main/java/com/planningInspectorate/ServiceLayer/databaseCrudLogic.java
@@ -1,16 +1,38 @@
 package com.planningInspectorate.ServiceLayer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import com.planningInspectorate.DataLayer.*;
 
-
+import javax.naming.directory.SearchResult;
+import java.util.Optional;
 
 @Service
 public class databaseCrudLogic {
 
+    @Autowired
+    private ContactRepository contactRepository;
+    private DepartmentRepository departmentRepository;
+    private OrganisationRepository organisationRepository;
+    private PersonRepository personRepository;
+    private SpecialContactRepository specialContactRepository;
 
+
+    @GetMapping
     public String GetRecords(String searchTerm){
 
         //TODO: add code here
+        //Search searchTerm from data layer
+        Optional<Object> foundContact = contactRepository.findAll(searchTerm);
+        Optional<Object> foundDepartment = departmentRepository.findAll(searchTerm);
+        Optional<Object> foundOrganization = organisationRepository.findAll(searchTerm);
+        Optional<Object> foundPerson = personRepository.findAll(searchTerm);
+        Optional<Object> foundSpecialContact =specialContactRepository.findAll(searchTerm);
 
+        // migrate all found data
+        //return the migrated data
+                //.orElseThrow(() -> ("Couldn't find term" + searchTerm));
 
         return ("Get records: "+searchTerm);
 
@@ -30,8 +52,8 @@ public class databaseCrudLogic {
 
     }
 
-    public String DeleteRecord(){
-
+    public String DeleteRecord(String contactId){
+        contactRepository.deleteById((long) Integer.parseInt(contactId));
         //TODO: add code here
         return "Delete Records";
 
