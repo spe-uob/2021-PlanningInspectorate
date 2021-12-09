61b41766c74db5d96f6e78b659d155426a12d98f
George Wigley <43220609+GeorgeWigley@users.noreply.github.com>
2021-12-09 21:09:48 +0000

    Revert "updating file structure to make deployment easier"

--- src/main/java/com/planningInspectorate/DataLayer/SpecialContactRepository.java
+++
@@ -1,9 +0,0 @@
-package com.planningInspectorate.DataLayer;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
-public interface SpecialContactRepository extends JpaRepository<SpecialContact, Long> {
-
-}
