8bfdc8db2c9c1406307abe166b1b1c93cb736907
Manami <manami2622@gmail.com>
2022-02-03 22:22:24 +0000

    added passwordEncoder

--- SpringBoot/planningInspectorate/src/main/java/com/planningInspectorate/Configuration/securityConfig.java
+++ SpringBoot/planningInspectorate/src/main/java/com/planningInspectorate/Configuration/securityConfig.java
@@ -1,11 +1,15 @@
 package com.planningInspectorate.Configuration;
 
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -20,6 +24,21 @@ public class securityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .formLogin();
     }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+        auth.inMemoryAuthentication()
+                .withUser("user")
+                .password(passwordEncoder().encode("password"))
+                .roles("USER");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+
 
 
 }
