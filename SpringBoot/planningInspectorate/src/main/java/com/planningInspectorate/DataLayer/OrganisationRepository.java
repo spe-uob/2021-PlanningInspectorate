8368beb542e572e49dd3426849691f1ce21b2b01
Imran Zamin Ali <izaminali@gmail.com>
2022-02-10 23:50:56 +0000

    Revert "Merge branch 'main' into dev"
    This reverts commit a70429de205677592537b7d661077e757107ddfb.

--- SpringBoot/planningInspectorate/src/main/java/com/planningInspectorate/DataLayer/OrganisationRepository.java
+++
@@ -1,6 +0,0 @@
-package com.planningInspectorate.DataLayer;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrganisationRepository extends JpaRepository<Organisation, Long> {
-}
