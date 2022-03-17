287b0ee8b3c21e0cf3cbb3a9dbd218e49d1facdf
George W <georgewigley123@gmail.com>
2022-03-17 18:05:47 +0000

    Cleaning up the repo: Deleted Branch: Unit Tests

--- src/main/java/com/planningInspectorate/Security/securityConfig.java
+++ src/main/java/com/planningInspectorate/Security/securityConfig.java
@@ -1,6 +1,5 @@
 package com.planningInspectorate.Security;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
