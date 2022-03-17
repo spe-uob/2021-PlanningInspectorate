d542e62baff13cd6ceeed9c91f340b32ac78371d
George W <georgewigley123@gmail.com>
2022-03-17 18:05:02 +0000

    Cleaning up the repo: Deleted Branch: Revert Branches

--- src/main/java/com/planningInspectorate/Security/securityConfig.java
+++ src/main/java/com/planningInspectorate/Security/securityConfig.java
@@ -1,5 +1,6 @@
 package com.planningInspectorate.Security;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
