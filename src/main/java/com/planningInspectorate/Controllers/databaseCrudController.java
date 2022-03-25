f384bb944ae0b67dc3763cb522e453921317df28
ImranZaminAli <izaminali@gmail.com>
2022-03-25 11:05:47 +0000

    finished verify function in controller level

--- src/main/java/com/planningInspectorate/Controllers/databaseCrudController.java
+++ src/main/java/com/planningInspectorate/Controllers/databaseCrudController.java
@@ -71,25 +71,11 @@ public class databaseCrudController {
 
     @GetMapping("/verifyOTP/{pin}")
     public boolean VerifyOTP(@PathVariable String pin){
+        return databaseCrudLogic.VerifyOTP(pin);
         // Imran can you implement this should return true or false for if the pin exists or doesn't
         // gonna use a second function to get the record data from a given pin
-        return true;
+        //return true;
     }
 
-    @GetMapping("/getRecordFromOTP/{pin}")
-    public CompleteRecord[] GetRecordFromOTP(@PathVariable String pin){
-        // This should return a record based on the OTP pin, basically identical to get record but searches OTP's
-        // Should only be used after VerifyOTP
-        return null;
-    }
 
-    @PutMapping("/updateOtp")
-    public boolean updateOtp(@RequestBody String[] data){
-        // another one, this gets sent when the user updates the data, the String[] data is an array of the new data
-        // it has the otp as the first field (should be used to identify the record that needs updating)
-        // it then has the values (no id) so sched 1, org name, apfp regs etc
-        // There is no spam handling on the frontend so once this is called it should also delete the otp for that record
-
-        return true;
-    }
 }
